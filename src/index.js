19ca4dc2efe92a129a559bd5cd8176282095724a
Chanakya sarma <chanakyasarmakashyap@gmail.com>
2025-09-25 14:37:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './dark-mode.css';
@@ -6,6 +6,9 @@ import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+useEffect(() => {
+  document.body.classList.add('dark-mode'); // <-- apply globally
+}, []);
 root.render(
   <React.StrictMode> 
       
